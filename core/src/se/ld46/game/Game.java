1ddc6a469179a4776c95a6d8825d2c662d004293
Andreas Brommund <andreas@brommund.se>
2020-04-18 15:28:19 +0200

    Fix merge

--- core/src/se/ld46/game/Game.java
+++ core/src/se/ld46/game/Game.java
@@ -7,37 +7,17 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import se.ld46.game.input.GameInputProcessor;
 
 import static se.ld46.game.AssetManagerBuilder.assetManagerBuilder;
 import static se.ld46.game.WorldCamera.worldCamera;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.math.MathUtils;
-import se.ld46.game.input.GameInputProcessor;
 
 public class Game extends ApplicationAdapter {
     private SpriteBatch batch;
     private AssetManager assetManager;
     private WorldCamera worldCamera;
     private MapRenderer mapRenderer;
-    SpriteBatch batch;
     private Orc orc;
-    Texture backgroundImage;
-    OrthogonalTiledMapRenderer orthogonalTiledMapRenderer;
-    AssetManager assetManager;
-    OrthographicCamera camera;
-    BitmapFont font;
-
-    static final int WORLD_WIDTH = 100;
-    static final int WORLD_HEIGHT = 100;
-
-    private float orcX = 320;
-    private float orcY = 240;
-    private float dx = 1;
-    private float speed = 10;
-
-
-    private float rotationSpeed = 0.5f;
 
     public static final GameInputProcessor INPUT_PROCESSOR = new GameInputProcessor();
 
@@ -47,9 +27,6 @@ public class Game extends ApplicationAdapter {
 
         batch = new SpriteBatch();
         orc = new Orc();
-        backgroundImage = new Texture("background.png");
-
-
 
         worldCamera = worldCamera();
         assetManager = assetManagerBuilder()
@@ -63,8 +40,6 @@ public class Game extends ApplicationAdapter {
 
     @Override
     public void render() {
-
-
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
