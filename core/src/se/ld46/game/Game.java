cc35e0b5f0fc83676e0acc2aad41727557efcc40
David Skeppstedt <david.skeppstedt@gmail.com>
2020-04-18 11:15:38 +0200

    Add background

--- core/src/se/ld46/game/Game.java
+++ core/src/se/ld46/game/Game.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class Game extends ApplicationAdapter {
     SpriteBatch batch;
     Texture orcImage;
+    Texture backgroundImage;
 
     private float orcX = 320;
     private float orcY = 240;
@@ -19,6 +20,7 @@ public class Game extends ApplicationAdapter {
     public void create() {
         batch = new SpriteBatch();
         orcImage = new Texture("orc.png");
+        backgroundImage = new Texture("background.png");
     }
 
     @Override
@@ -38,6 +40,7 @@ public class Game extends ApplicationAdapter {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
+        batch.draw(backgroundImage, 0, 0);
         batch.draw(orcImage, orcX, 240);
         batch.end();
     }
