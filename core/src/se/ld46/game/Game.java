47f32a412d7c3e7ecdb3e5ce5a6443493c5a0aa2
David Skeppstedt <david.skeppstedt@gmail.com>
2020-04-18 09:44:09 +0200

    Add orc that moves

--- core/src/se/ld46/game/Game.java
+++ core/src/se/ld46/game/Game.java
@@ -7,27 +7,44 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Game extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    SpriteBatch batch;
+    Texture orcImage;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    private float orcX = 320;
+    private float orcY = 240;
+    private float dx = 1;
+    private float speed = 10;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        orcImage = new Texture("orc.png");
+    }
+
+    @Override
+    public void render() {
+
+        if (orcX + 32 > 640 || orcX < 0) {
+            dx = dx * -1;
+        }
+
+
+        System.out.println(orcX);
+
+
+        orcX += speed * dx;
+
+
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(orcImage, orcX, 240);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        orcImage.dispose();
+    }
 }
