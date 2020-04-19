695539af566d2fb382d5e016aad9fcf0410bfa11
Andreas Brommund <andreas@brommund.se>
2020-04-19 13:46:17 +0200

    Move with WASD

--- core/src/se/ld46/game/systems/CameraControlSystem.java
+++ core/src/se/ld46/game/systems/CameraControlSystem.java
@@ -2,7 +2,6 @@ package se.ld46.game.systems;
 
 import com.badlogic.ashley.core.EntitySystem;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.MathUtils;
 import se.ld46.game.input.GameInputProcessor;
@@ -11,6 +10,7 @@ import se.ld46.game.util.WorldCamera;
 
 import java.util.HashMap;
 
+import static com.badlogic.gdx.Input.Keys;
 import static se.ld46.game.util.Config.WORLD_HEIGHT;
 import static se.ld46.game.util.Config.WORLD_WIDTH;
 
@@ -20,7 +20,7 @@ public class CameraControlSystem extends EntitySystem implements KeyDownSubscrib
     public static final float zoomRate = 0.05f;
 
     private OrthographicCamera camera;
-    private HashMap<Integer, Boolean> keyDown = new HashMap<>();
+    private HashMap<Controls, Boolean> keyDown = new HashMap<>();
 
     public CameraControlSystem(int priority, WorldCamera worldCamera) {
         super(priority);
@@ -32,43 +32,40 @@ public class CameraControlSystem extends EntitySystem implements KeyDownSubscrib
     public void update(final float deltaTime) {
         super.update(deltaTime);
         handleInput();
-
-
         camera.update();
     }
 
     @Override
     public void onKeyDown(final int keycode) {
-        keyDown.put(keycode, true);
+        keyDown.put(Controls.fromKeycode(keycode), true);
         Gdx.app.debug("CameraControlSystem", "" + keycode);
     }
 
     private void handleInput() {
 
-
-        if (keyDown.getOrDefault(Input.Keys.S, false)) {
+        if (keyDown.getOrDefault(Controls.ZOOM_OUT, false)) {
             camera.zoom += zoomRate;
-            keyDown.put(Input.Keys.S, false);
+            keyDown.put(Controls.ZOOM_OUT, false);
         }
-        if (keyDown.getOrDefault(Input.Keys.W, false)) {
+        if (keyDown.getOrDefault(Controls.ZOOM_IN, false)) {
             camera.zoom -= zoomRate;
-            keyDown.put(Input.Keys.W, false);
+            keyDown.put(Controls.ZOOM_IN, false);
         }
-        if (keyDown.getOrDefault(Input.Keys.LEFT, false)) {
+        if (keyDown.getOrDefault(Controls.MOVE_LEFT, false)) {
             camera.translate(-tilesToMove, 0, 0);
-            keyDown.put(Input.Keys.LEFT, false);
+            keyDown.put(Controls.MOVE_LEFT, false);
         }
-        if (keyDown.getOrDefault(Input.Keys.RIGHT, false)) {
+        if (keyDown.getOrDefault(Controls.MOVE_RIGHT, false)) {
             camera.translate(tilesToMove, 0, 0);
-            keyDown.put(Input.Keys.RIGHT, false);
+            keyDown.put(Controls.MOVE_RIGHT, false);
         }
-        if (keyDown.getOrDefault(Input.Keys.DOWN, false)) {
+        if (keyDown.getOrDefault(Controls.MOVE_DOWN, false)) {
             camera.translate(0, -tilesToMove, 0);
-            keyDown.put(Input.Keys.DOWN, false);
+            keyDown.put(Controls.MOVE_DOWN, false);
         }
-        if (keyDown.getOrDefault(Input.Keys.UP, false)) {
+        if (keyDown.getOrDefault(Controls.MOVE_UP, false)) {
             camera.translate(0, tilesToMove, 0);
-            keyDown.put(Input.Keys.UP, false);
+            keyDown.put(Controls.MOVE_UP, false);
         }
 
         camera.zoom = MathUtils.clamp(camera.zoom, 0.1f, 100 / camera.viewportWidth);
@@ -78,6 +75,39 @@ public class CameraControlSystem extends EntitySystem implements KeyDownSubscrib
 
         camera.position.x = MathUtils.clamp(camera.position.x, effectiveViewportWidth / 2f, WORLD_WIDTH - effectiveViewportWidth / 2f);
         camera.position.y = MathUtils.clamp(camera.position.y, effectiveViewportHeight / 2f, WORLD_HEIGHT - effectiveViewportHeight / 2f);
+    }
+
+    private enum Controls {
+        ZOOM_OUT,
+        ZOOM_IN,
+        MOVE_UP,
+        MOVE_DOWN,
+        MOVE_LEFT,
+        MOVE_RIGHT,
+        NONE;
+
+        public static Controls fromKeycode(int keycode) {
+            switch (keycode) {
+                case Keys.UP:
+                case Keys.W:
+                    return MOVE_UP;
+                case Keys.DOWN:
+                case Keys.S:
+                    return MOVE_DOWN;
+                case Keys.RIGHT:
+                case Keys.D:
+                    return MOVE_RIGHT;
+                case Keys.LEFT:
+                case Keys.A:
+                    return MOVE_LEFT;
+                case Keys.E:
+                    return ZOOM_IN;
+                case Keys.Q:
+                    return ZOOM_OUT;
+                default:
+                    return NONE;
+            }
+        }
     }
 
 }
